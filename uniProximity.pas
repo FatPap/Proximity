f58805f8cc919dde6d6bba022d839b9136117bc6
Christian Dallinger <dallinger_christian@hotmail.com>
2022-02-17 00:24:16 +0100

    added empty, red and blue status and corresponding colors

--- uniProximity.pas
+++ uniProximity.pas
@@ -26,17 +26,27 @@ type
 
   end;
 
+  THexFieldStatus = ( none,
+                      empty,
+                      red,
+                      blue  );
+
   THexField = class(TCustomPath)
   private
     fTxt: TText;
+    fHexFieldStatus: THexFieldStatus;
+    procedure SetHexFieldStatus(const Value: THexFieldStatus);
   public
     constructor Create(AOwner: TComponent); override;
+    property HexFieldStatus: THexFieldStatus read fHexFieldStatus write SetHexFieldStatus;
+
   end;
 
 implementation
 
 uses
-  System.UIConsts;
+  System.UIConsts,
+  FMX.Graphics;
 
 { TProximity }
 
@@ -44,7 +54,11 @@ constructor TProximity.Create(AOwner: TComponent);
 var
   x, y: Integer;
   px, py: Single;
-  hf: THexField;
+const
+ coStrokeThickness = 1;
+ coSizeFull = 120;
+ coSizeHalf = coSizeFull div 2;
+ coSize34 = coSizeFull div 4 * 3;
 begin
   inherited;
   Align := TAlignLayout.Client;
@@ -56,18 +70,19 @@ begin
     begin
       if y mod 2 = 0 then
       begin
-        px := x * 150;
-        py := y * 50;
+        px := x * (coSizeFull + coSizeHalf) - (2 * x * coStrokeThickness);
+        py := y * (coSizeHalf - coStrokeThickness);
       end else
       begin
-        px := x * 150 + 75;
-        py := y * 50;
+      //px := x * (coSizeFull + coSizeHalf - coStrokeThickness) + (coSize34 - coStrokeThickness);
+        px := x * (coSizeFull + coSizeHalf) + (coSize34 - coStrokeThickness) - (2 * x * coStrokeThickness);
+        py := y * (coSizeHalf - coStrokeThickness);
       end;
 
       fHexFields[x, y] := THexField.Create(Self);
       fHexFields[x, y].fTxt.Text := x.ToString + ',' + y.ToString;
-      fHexFields[x, y].Width := 100;
-      fHexFields[x, y].Height := 100;
+      fHexFields[x, y].Width := coSizeFull;
+      fHexFields[x, y].Height := coSizeFull;
       fHexFields[x, y].Position.X := px;
       fHexFields[x, y].Position.Y := py;
       fHexFields[x, y].HitTest := True;
@@ -88,7 +103,14 @@ var
   hf: THexField;
 begin
   hf := THexField(Sender);
-  hf.fTxt.Text := random(99).ToString;
+
+  case hf.HexFieldStatus of
+    empty: hf.HexFieldStatus := THexFieldStatus.red;
+    red  : hf.HexFieldStatus := THexFieldStatus.blue;
+    blue : hf.HexFieldStatus := THexFieldStatus.empty;
+  end;
+
+  //hf.fTxt.Text := random(99).ToString;
 end;
 
 
@@ -98,13 +120,70 @@ constructor THexField.Create(AOwner: TComponent);
 begin
   inherited;
   // Source: https://stackoverflow.com/questions/34417564/how-do-i-create-a-cut-out-hexagon-shape
-  Data.Data := 'M2.5,0.66 L7.5,0.66 L10,5 L7.5,9.33 L2.5,9.33 L0,5 z';
+  //Data.Data := 'M2.5,0.66 L7.5,0.66 L10,5 L7.5,9.33 L2.5,9.33 L0,5 z';
+  Data.Data := 'M2.5,0 L7.5,0 L10,5 L7.5,10 L2.5,10 L0,5 z';
+
+  Stroke.Color := claBlack;
+  StrokeThickness := 2;
+
+  Fill.Kind := TBrushKind.Gradient;
+
+  fHexFieldStatus := THexFieldStatus.none;
+  HexFieldStatus := THexFieldStatus.empty;
 
   fTxt := TText.Create(Self);
   fTxt.Align := TAlignLayout.Client;
   fTxt.HitTest := False;
-  fTxt.TextSettings.FontColor := claBlue;
+  fTxt.TextSettings.Font.Size := fTxt.TextSettings.Font.Size + 20;
+  fTxt.TextSettings.Font.Style := fTxt.TextSettings.Font.Style + [tfontstyle.fsBold];
+  fTxt.TextSettings.FontColor := claBlack;
   fTxt.Parent := Self;
+end;
+
+procedure THexField.SetHexFieldStatus(const Value: THexFieldStatus);
+begin
+  if fHexFieldStatus <> Value then
+  begin
+    fHexFieldStatus := Value;
+
+    case fHexFieldStatus of
+      none:;
+      empty:  begin
+                Fill.Gradient.Style := TGradientStyle.Radial;
+                Fill.Gradient.Points[0].Color := $FF37615C;
+                Fill.Gradient.Points[0].Offset := 0;
+                Fill.Gradient.Points[1].Color := $FF508E86;
+                Fill.Gradient.Points[1].Offset := 1;
+              end;
+      red  :  begin
+                Fill.Gradient.Style := TGradientStyle.Linear;
+
+                Fill.Gradient.StartPosition.Y := 0.733153820037841800;
+                Fill.Gradient.StopPosition.X  := 1.000000000000000000;
+                Fill.Gradient.StopPosition.Y  := 0.266846150159835800;
+
+                Fill.Gradient.Points[0].Color := $FFBC0000;
+                Fill.Gradient.Points[0].Offset := 0.801242232322692900;
+                Fill.Gradient.Points[1].Color := $FFFBD7D7;
+                Fill.Gradient.Points[1].Offset := 0.953416168689727700;
+              end;
+
+      blue :  begin
+                Fill.Gradient.Style := TGradientStyle.Linear;
+
+                Fill.Gradient.StartPosition.Y := 0.733153820037841800;
+                Fill.Gradient.StopPosition.X  := 1.000000000000000000;
+                Fill.Gradient.StopPosition.Y  := 0.266846150159835800;
+
+                Fill.Gradient.Points[0].Color := $FF1312C9;
+                Fill.Gradient.Points[0].Offset := 0.801242232322692900;
+                Fill.Gradient.Points[1].Color := $FFDCDCFC;
+                Fill.Gradient.Points[1].Offset := 0.953416168689727700;
+              end;
+    end;
+
+    Repaint;
+  end;
 end;
 
 end.
